8e4aadd3c64e125c459e5610958c57c458358547
Ryan Wang <i@ryanc.cc>
2022-09-25 21:53:31 +0800

    refactor: migrate template files
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./styles/main.less"
 import "nprogress/nprogress.css"
 import NProgress from "nprogress"
+import '@purge-icons/generated'
 
 NProgress.start();
 document.addEventListener("DOMContentLoaded", function () {
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
 import path from "path";
 import { fileURLToPath } from "url";
+import PurgeIcons from "vite-plugin-purge-icons";
 
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    PurgeIcons({
+      content: ["./templates/**/*.html"],
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
       name: "main",
       fileName: "main",
-      formats: ["umd"],
+      formats: ["iife"],
     },
     outDir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
     emptyOutDir: true,
